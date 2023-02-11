b8a82f3303c5f2cdd264f866635abbc67071607e
Nishantkishu8 <122088180+Nishantkishu8@users.noreply.github.com>
2023-02-11 12:03:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,21 +7,43 @@ let arr = [
 ];
 
 function PrintStudentswithMap() {
-  //Write your code here , just console.log
-}
+   arr.map(a => {
+      if (a.marks > 50) {
+       console.log(a);
+      }
+    });
+  }
 
 function PrintStudentsbyForEach() {
+  arr.forEach(a => {
+      if (a.marks > 50) {
+        console.log(a);
+      }
+    });
   //Write your code here , just console.log
 }
 
 function addData() {
+   let newArr = { id: 4, name: "susan", age: "20", marks: 45 };
+    arr.push(newArr);
+    console.log(arr);
   //Write your code here, just console.log
 }
 
 function removeFailedStudent() {
+  let filteredArr = arr.filter(a => a.marks >= 50);
+    console.log(filteredArr);
   //Write your code here, just console.log
 }
 
 function concatenateArray() {
+  let arr1 = [
+      { id: 7, name: "Umesh bhai", age: "20", marks: 85 },
+      { id: 9, name: "Aman rock", age: "20", marks: 85 },
+      { id: 11, name: "panther", age: "20", marks: 85 },
+    ];
+    let all = arr.concat(arr1);
+  
+  console.log(all)
   //Write your code here, just console.log
 }
